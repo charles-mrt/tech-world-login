20551c64d390fc760c53a78fb01c96bb47456e47
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-12 13:50:34 -0300

    feat: added focus highlighting

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from "react"
+'use client'
+
+import { ReactElement, useState } from "react"
 
 interface InputProps {
   icon?: ReactElement
@@ -7,11 +9,35 @@ interface InputProps {
 }
 
 
-export const InputField = ({ icon, type, placeHolder}: InputProps) => {
+export const InputField = ({ icon, type, placeHolder }: InputProps) => {
+
+  const [isActived, setIsActived] = useState(Boolean)
+
+  const handleInputFocus = (actived: boolean) => {
+    setIsActived(actived)
+  }
+
   return (
-    <div className=" w-full h-10 rounded-full shadow-custom-input bg-white flex items-center gap-3 px-3">
+    <div className={`
+      w-full 
+      h-10 
+      rounded-full 
+      shadow-custom-input
+    bg-white 
+    text-gray-700 
+      flex 
+      items-center 
+      gap-3
+      px-3 
+      overflow-hidden
+      border-2
+      transition duration-150 ease-in
+      ${isActived ? "border-violet-500" : "" }`}
+    >
       <label className="text-violet-500" >{icon}</label>
       <input
+        onFocus={() => handleInputFocus(true)}
+        onBlur={() => handleInputFocus(false)}
         type={type}
         placeholder={placeHolder}
         className=" w-full h-full border-0 focus:outline-none"
