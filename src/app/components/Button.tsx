d54b9510e8577671b709e82dd70d5526e0bec4ec
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-12 13:51:21 -0300

    style: added transition and hover effects

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -5,7 +5,7 @@ export const Button = ({ text }: buttonProps) => {
   return (
     <button
       type="submit"
-      className="bg-violet-500 p-2 w-1/2 rounded-full text-white text-lg">
+      className="bg-violet-500 p-2 w-1/2 rounded-full text-white text-lg transition duration-150 ease-in hover:bg-violet-600">
       {text}
     </button>
   )
