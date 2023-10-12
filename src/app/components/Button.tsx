432b5dd1d949489c229437b3826465d53219eb0a
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-12 16:13:14 -0300

    feat: Add function to handle clicks on the submit button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,11 +1,14 @@
 interface buttonProps {
   text: string
+  handleClick?: () => void
 }
-export const Button = ({ text }: buttonProps) => {
+export const Button = ({ text, handleClick }: buttonProps) => {
   return (
     <button
       type="submit"
-      className="bg-violet-500 p-2 w-1/2 rounded-full text-white text-lg transition duration-150 ease-in hover:bg-violet-600">
+      className="bg-violet-500 p-2 w-1/2 rounded-full text-white text-lg transition duration-150 ease-in hover:bg-violet-600"
+      onClick={handleClick}
+      >
       {text}
     </button>
   )
