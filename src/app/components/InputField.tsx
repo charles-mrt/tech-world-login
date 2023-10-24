7c19c89cabb4d515a4c3e49e9686582f0c540293
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-24 17:09:57 -0300

    refactor: refactor typescript

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { ReactElement, useState } from "react"
+import { ReactElement, useState, ForwardedRef, forwardRef } from "react"
+import { UseFormRegister, FieldValues } from 'react-hook-form';
 
 interface InputProps {
   icon?: ReactElement
   type: string
   placeHolder?: string
   value?: string
+  nameValue?: string
+  handleOnChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  inputRef?: ForwardedRef<HTMLInputElement>
+  register?: UseFormRegister<FieldValues>
 }
 
 
-export const InputField = ({ icon, type, placeHolder, value }: InputProps) => {
+export const InputField = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
 
   const [isActived, setIsActived] = useState(Boolean)
 
@@ -33,17 +38,21 @@ export const InputField = ({ icon, type, placeHolder, value }: InputProps) => {
       overflow-hidden
       border-2
       transition duration-150 ease-in
-      ${isActived ? "border-violet-500" : "" }`}
+      ${isActived ? "border-violet-500" : ""}`}
     >
-      <label className="text-violet-500" >{icon}</label>
+      <label className="text-violet-500" >{props.icon}</label>
       <input
         onFocus={() => handleInputFocus(true)}
         onBlur={() => handleInputFocus(false)}
-        type={type}
-        placeholder={placeHolder}
-        value={value}
+        type={props.type}
+        placeholder={props.placeHolder}
+        value={props.value}
+        onChange={props.handleOnChange}
+        ref={ref}
+        {...props.register}
+        name={props.nameValue}
         className=" w-full h-full border-0 focus:outline-none"
       />
     </div>
   )
-}
+})
