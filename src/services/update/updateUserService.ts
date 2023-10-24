05b6ecbaa458cdb8950eee75c890feb16f65fd2f
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-24 17:26:15 -0300

    refactor: code refactor

--- src/services/update/updateUserService.ts
+++ src/services/update/updateUserService.ts
@@ -3,12 +3,9 @@ import { database, UserProps } from "../../data/database"
 
 export const updateUser = ({ id, ...updatedUserData }: UserProps) => {
 
-  const findEmail = database.find(user => user.email === updatedUserData.email)
+  const findEmail = database.find(user => user.email === updatedUserData.email && user.id !== id)
+
   const user = database.find(user => user.id === id)
 
-  if (!findEmail && user) { 
-    Object.assign(user, updatedUserData)
-  } else {
-    if (!user) throw new Error('user not find')
-  }
+  if (!findEmail && user) Object.assign(user, updatedUserData)
 }
