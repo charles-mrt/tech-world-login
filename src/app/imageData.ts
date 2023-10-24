92989d454e7c7dce2117623f5fcb3d9cd4b592a9
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-24 17:06:15 -0300

    feat: background image data

---
+++ src/app/imageData.ts
@@ -0,0 +1,157 @@
+import image_1 from '../../public/assets/background/image-1.jpg'
+import image_2 from '../../public/assets/background/image-2.jpg'
+import image_3 from '../../public/assets/background/image-3.jpg'
+import image_4 from '../../public/assets/background/image-4.jpg'
+import image_5 from '../../public/assets/background/image-5.jpg'
+import image_6 from '../../public/assets/background/image-6.jpg'
+import image_7 from '../../public/assets/background/image-7.jpg'
+import image_8 from '../../public/assets/background/image-8.jpg'
+import image_9 from '../../public/assets/background/image-9.jpg'
+import image_10 from '../../public/assets/background/image-10.jpg'
+import image_11 from '../../public/assets/background/image-11.jpg'
+import image_12 from '../../public/assets/background/image-12.jpg'
+import image_13 from '../../public/assets/background/image-13.jpg'
+import image_14 from '../../public/assets/background/image-14.jpg'
+import image_15 from '../../public/assets/background/image-15.jpg'
+import image_16 from '../../public/assets/background/image-16.jpg'
+import image_17 from '../../public/assets/background/image-17.jpg'
+import image_18 from '../../public/assets/background/image-18.jpg'
+import image_19 from '../../public/assets/background/image-19.jpg'
+import image_20 from '../../public/assets/background/image-20.jpg'
+import image_21 from '../../public/assets/background/image-21.jpg'
+import image_22 from '../../public/assets/background/image-22.jpg'
+import image_23 from '../../public/assets/background/image-23.jpg'
+import image_24 from '../../public/assets/background/image-24.jpg'
+import image_25 from '../../public/assets/background/image-25.jpg'
+import image_26 from '../../public/assets/background/image-26.jpg'
+import image_27 from '../../public/assets/background/image-27.jpg'
+import image_28 from '../../public/assets/background/image-28.jpg'
+import image_29 from '../../public/assets/background/image-29.jpg'
+import image_30 from '../../public/assets/background/image-30.jpg'
+import image_31 from '../../public/assets/background/image-31.jpg'
+import image_32 from '../../public/assets/background/image-32.jpg'
+import image_33 from '../../public/assets/background/image-33.jpg'
+import image_34 from '../../public/assets/background/image-34.jpg'
+import image_35 from '../../public/assets/background/image-35.jpg'
+import image_36 from '../../public/assets/background/image-36.jpg'
+import image_37 from '../../public/assets/background/image-37.jpg'
+import image_38 from '../../public/assets/background/image-38.jpg'
+import image_39 from '../../public/assets/background/image-39.jpg'
+import image_40 from '../../public/assets/background/image-40.jpg'
+import image_41 from '../../public/assets/background/image-41.jpg'
+import image_42 from '../../public/assets/background/image-42.jpg'
+import image_43 from '../../public/assets/background/image-43.jpg'
+import image_44 from '../../public/assets/background/image-44.jpg'
+import image_45 from '../../public/assets/background/image-45.jpg'
+import image_46 from '../../public/assets/background/image-46.jpg'
+import image_47 from '../../public/assets/background/image-47.jpg'
+import image_48 from '../../public/assets/background/image-48.jpg'
+import image_49 from '../../public/assets/background/image-49.jpg'
+import image_50 from '../../public/assets/background/image-50.jpg'
+import image_51 from '../../public/assets/background/image-51.jpg'
+import image_52 from '../../public/assets/background/image-52.jpg'
+import image_53 from '../../public/assets/background/image-53.jpg'
+import image_54 from '../../public/assets/background/image-54.jpg'
+import image_55 from '../../public/assets/background/image-55.jpg'
+import image_56 from '../../public/assets/background/image-56.jpg'
+import image_57 from '../../public/assets/background/image-57.jpg'
+import image_58 from '../../public/assets/background/image-58.jpg'
+import image_59 from '../../public/assets/background/image-59.jpg'
+import image_60 from '../../public/assets/background/image-60.jpg'
+import image_61 from '../../public/assets/background/image-61.jpg'
+import image_62 from '../../public/assets/background/image-62.jpg'
+import image_63 from '../../public/assets/background/image-63.jpg'
+import image_64 from '../../public/assets/background/image-64.jpg'
+import image_65 from '../../public/assets/background/image-65.jpg'
+import image_66 from '../../public/assets/background/image-66.jpg'
+import image_67 from '../../public/assets/background/image-67.jpg'
+import image_68 from '../../public/assets/background/image-68.jpg'
+import image_69 from '../../public/assets/background/image-69.jpg'
+import image_70 from '../../public/assets/background/image-70.jpg'
+import image_71 from '../../public/assets/background/image-71.jpg'
+import image_72 from '../../public/assets/background/image-72.jpg'
+import image_73 from '../../public/assets/background/image-73.jpg'
+import image_74 from '../../public/assets/background/image-74.jpg'
+import image_75 from '../../public/assets/background/image-75.jpg'
+import image_76 from '../../public/assets/background/image-76.jpg'
+import image_77 from '../../public/assets/background/image-77.jpg'
+
+export const imageData = [
+  image_1,
+  image_2,
+  image_3,
+  image_4,
+  image_5,
+  image_6,
+  image_7,
+  image_8,
+  image_9,
+  image_10,
+  image_11,
+  image_12,
+  image_13,
+  image_14,
+  image_15,
+  image_16,
+  image_17,
+  image_18,
+  image_19,
+  image_20,
+  image_21,
+  image_22,
+  image_23,
+  image_24,
+  image_25,
+  image_26,
+  image_27,
+  image_28,
+  image_29,
+  image_30,
+  image_31,
+  image_32,
+  image_33,
+  image_34,
+  image_35,
+  image_36,
+  image_37,
+  image_38,
+  image_39,
+  image_40,
+  image_41,
+  image_42,
+  image_43,
+  image_44,
+  image_45,
+  image_46,
+  image_47,
+  image_48,
+  image_49,
+  image_50,
+  image_51,
+  image_52,
+  image_53,
+  image_54,
+  image_55,
+  image_56,
+  image_57,
+  image_58,
+  image_59,
+  image_60,
+  image_61,
+  image_62,
+  image_63,
+  image_64,
+  image_65,
+  image_66,
+  image_67,
+  image_68,
+  image_69,
+  image_70,
+  image_71,
+  image_72,
+  image_73,
+  image_74,
+  image_75,
+  image_76,
+  image_77
+]
