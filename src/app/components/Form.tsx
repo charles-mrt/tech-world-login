126fa90f8b07ab6d788c7c48456bc67b633889d9
charles-mrt <charlesmartins.ads@gmail.com>
2024-01-15 16:43:00 -0300

    refactor: removed unnecessary react hook form imports

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FormSchema, zodSchema } from "../zodSchema/schema" 
 
